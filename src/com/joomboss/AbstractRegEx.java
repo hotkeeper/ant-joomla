d69715b2a04dc20a8de149332ecb3f0f2270fdd5
hotkeeper <andreas.kessler@comfone.com>
2016-11-24 10:04:10 +0100

    Logout success expression adapted to Joomla 3.6.4

--- src/com/joomboss/AbstractRegEx.java
+++ src/com/joomboss/AbstractRegEx.java
@@ -4,7 +4,7 @@ abstract class AbstractRegEx implements RegEx {
 	private static final String GET_TOKEN_REGEX = "<input type=\"hidden\" name=\"([a-z0-9]+)\" value=\"1\" />";
 	private static final String GET_ERROR_MESSAGE_REGEX = "<dd class=\"error message\">\\s*<ul>\\s*<li>(.*?)</li>";
 	private final static String UNINSTALLION_WAS_SUCCESSFULL_REGEG_EN = "Uninstalling [\\p{L}0-9]* was successful.";
-	private final static String LOGOUT_WAS_SUCCESSFULL_REGEG_EN = "User Name|Benutzername";
+	private final static String LOGOUT_WAS_SUCCESSFULL_REGEG_EN = "Username|Benutzername";
 
 	@Override
 	public String getGET_TOKEN_REGEX() {
